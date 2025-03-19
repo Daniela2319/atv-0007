40aa69bf700cdd1e238070e514ab75bde1fff9e6
Daniela2319 <frankvt04@gmail.com>
2025-03-19 14:49:30 -0300

    Criado os dados da variavel veiculo

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 // As variáveis representa veículos
-const veiculo = {
-  marca: "marca",
-  modelo: "modelo",
-  placa: "placa",
-  descricao: "descricao",
-  cor: "cor",
-  km: "km",
-}
+let veiculo;
+let marca;
+let modelo;
+let placa;
+let descricao;
+let cor;
+let km;
+
+//Dados do veículo 1
+veiculo = 1;
+marca = "volkswagen";
+modelo = "Fusca";
+placa = "XYZ-1234";
+descricao = "Carro antigo e conservado";
+cor = "Azul";
+km = 5000;
+console.log(marca, modelo, placa, descricao, cor, km);
+
+//Dados do veículo 2
+veiculo = 2;
+marca = "Fiat";
+modelo = "Palio";
+placa = "DEF-4567";
+descricao = "Carro econômico";
+cor = "Vermelho";
+km = 50000;
+console.log(marca, modelo, placa, descricao, cor, km);
+
+//Dados do veículo 3
+veiculo = 3;
+marca = "Toyota";
+modelo = "Corolla";
+placa = "ABC-1234";
+descricao = "Carro confortável, ideal para família";
+cor = "Prata";
+km = 45000;
+
+console.log(marca, modelo, placa, descricao, cor, km);
+
+
+
 
 
